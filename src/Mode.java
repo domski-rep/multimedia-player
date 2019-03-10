f48a83ca43d1bbc5f8bfe684c8015b23cae9df71
Bartek <ebartosz.d@gmail.com>
2019-03-10 16:57:52 +0100

    Added all functionality of media player

---
+++ src/Mode.java
@@ -0,0 +1,5 @@
+public enum Mode {
+    SHUFFLE,
+    LOOP,
+    SEQUENT
+}
---
+++ src/Movie.java
@@ -0,0 +1,21 @@
+public class Movie extends Track implements Playable{
+
+    public Movie(String name) {
+        super(name);
+    }
+
+    @Override
+    public void play() {
+        System.out.println(this);
+        try {
+            Thread.sleep(1200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String toString(){
+        return "Film: " + super.toString();
+    }
+}
